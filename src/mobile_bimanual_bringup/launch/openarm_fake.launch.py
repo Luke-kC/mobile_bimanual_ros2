181f9eab3acb4b14153d00127e7e4dde9f9f27fa
Luke Chen <43226681+Luke-kC@users.noreply.github.com>
2026-08-30 00:39:41 -0400

    change arm type to v1.0

--- src/mobile_bimanual_bringup/launch/openarm_fake.launch.py
+++ src/mobile_bimanual_bringup/launch/openarm_fake.launch.py
@@ -6,21 +6,25 @@ from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
-    upstream_launch = PathJoinSubstitution([
-        FindPackageShare('openarm_bringup'),
-        'launch',
-        'openarm.bimanual.launch.py',
-    ])
+    upstream_launch = PathJoinSubstitution(
+        [
+            FindPackageShare("openarm_bringup"),
+            "launch",
+            "openarm.bimanual.launch.py",
+        ]
+    )
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(upstream_launch),
-            launch_arguments={
-                'arm_type': 'v2.0',
-                'use_fake_hardware': 'true',
-                'robot_controller': 'forward_position_controller',
-                'right_can_interface': 'can0',
-                'left_can_interface': 'can1',
-            }.items(),
-        ),
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(upstream_launch),
+                launch_arguments={
+                    "arm_type": "v1.0",
+                    "use_fake_hardware": "true",
+                    "robot_controller": "forward_position_controller",
+                    "right_can_interface": "can0",
+                    "left_can_interface": "can1",
+                }.items(),
+            ),
+        ]
+    )
